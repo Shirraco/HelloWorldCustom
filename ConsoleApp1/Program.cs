03703a9040a61d9b6ac22514435876d60efef32b
Jared <randomcan1@gmail.com>
2021-03-29 15:19:02 -0500

    Push Test 3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello There! Whats your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Your name is " + userName + "? Nice to meet you!");
+            Console.ReadKey();
+            Console.WriteLine("See You Later, " + userName + "!");
+            Console.ReadKey();
+
+        }
+    }
+}
